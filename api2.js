4c8a93376fb13178c0157fb63ffec689dadbaaf8
Angel Arias <150385139+angelariasus@users.noreply.github.com>
2024-12-12 01:38:03 -0500

    Update api2.js

--- api2.js
+++ api2.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');  
 
+app.use(cors());
 app.use(bodyParser.json());
 
-let contador = { suben: 0, bajan: 0 };
-
-app.use(cors()); // Permitir solicitudes desde cualquier origen
-app.use(bodyParser.json());
+let contador = {
+  suben: 0,
+  bajan: 0,
+  paradero: 0,
+  subenParadero: 0,
+  bajanParadero: 0
+};
 
 app.get('/contador', (req, res) => {
   res.json(contador);
@@ -18,16 +21,36 @@ app.get('/contador', (req, res) => {
 
 app.post('/contador/suben', (req, res) => {
   contador.suben++;
+  if (contador.paradero === 0) {
+    contador.subenParadero++;
+  }
   res.json(contador);
 });
 
 app.post('/contador/bajan', (req, res) => {
   contador.bajan++;
+  if (contador.paradero === 0) {
+    contador.bajanParadero++;
+  }
   res.json(contador);
 });
 
 app.post('/contador/reiniciar', (req, res) => {
-  contador = { suben: 0, bajan: 0 };
+  contador.suben = 0;
+  contador.bajan = 0;
+  contador.subenParadero = 0;
+  contador.bajanParadero = 0;
+  res.json(contador);
+});
+
+app.post('/contador/paradero', (req, res) => {
+  const { estado } = req.body;
+  console.log(`Estado recibido: ${estado}`);  
+  if (estado === 0) {
+    contador.subenParadero = 0;
+    contador.bajanParadero = 0;
+  }
+  contador.paradero = estado;
   res.json(contador);
 });
 
